eb570a7e230329fd6391167ebbadfa422d6c5c99
Samuel Deya <samueldeya@gmail.com>
2025-01-14 15:04:16 +0300

    Initial commit

---
+++ util/db.js
@@ -0,0 +1,147 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+import { Sequelize, DataTypes } from "sequelize";
+import mongoose from "mongoose";
+
+// mongodb Database connection
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(
+    "mongodb+srv://samueldeya:m0t0m0t0@cluster0.qd7gjoz.mongodb.net/docs-medicare-app?retryWrites=true&w=majority",
+    {}
+  );
+}
+
+const docSchema = new mongoose.Schema({
+  Doctor_id: String,
+  username: String,
+  password: String,
+  googleToken: String,
+  googleEmail: String,
+});
+export const Doc = mongoose.model("Doc", docSchema);
+
+const adminSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+});
+
+export const Admin = mongoose.model("Admin", adminSchema);
+
+// mysql Database connection
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    port: process.env.PORT,
+  }
+);
+// Define models
+export const user = sequelize.define(
+  "user",
+  {
+    user_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: DataTypes.STRING,
+    age: DataTypes.STRING,
+    phone_number: DataTypes.STRING,
+    location: DataTypes.STRING,
+  },
+  {
+    timestamps: false,
+  }
+);
+
+export const doctor = sequelize.define(
+  "doctor",
+  {
+    doctor_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: DataTypes.STRING,
+    contact_info: {
+      type: DataTypes.STRING,
+      unique: true,
+    },
+    type: DataTypes.STRING,
+    location: DataTypes.STRING,
+    email: DataTypes.STRING,
+    address: DataTypes.STRING,
+  },
+  {
+    timestamps: false,
+  }
+);
+
+export const appointment = sequelize.define(
+  "appointment",
+  {
+    appointment_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    user_id: DataTypes.INTEGER,
+    doctor_id: DataTypes.INTEGER,
+    date: DataTypes.DATE,
+    time: DataTypes.STRING,
+    status: DataTypes.STRING,
+  },
+  {
+    timestamps: false,
+  }
+);
+export const teleappointment = sequelize.define(
+  "teleappointment",
+  {
+    appointment_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    user_id: DataTypes.INTEGER,
+    doctor_id: DataTypes.INTEGER,
+    date: DataTypes.DATE,
+    time: DataTypes.STRING,
+    status: DataTypes.STRING,
+  },
+  {
+    timestamps: false,
+  }
+);
+
+export const googleappointment = sequelize.define(
+  "googleappointment",
+  {
+    appointment_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    doctor_id: DataTypes.INTEGER,
+    date: DataTypes.DATEONLY,
+    start_time: DataTypes.TIME,
+    end_time: DataTypes.TIME,
+    appointment_info: DataTypes.STRING,
+    google_id: DataTypes.STRING,
+  },
+  {
+    timestamps: false,
+  }
+);
+
+// Define associations
+appointment.belongsTo(user, { foreignKey: "user_id", as: "User" });
+appointment.belongsTo(doctor, { foreignKey: "doctor_id", as: "Doctor" });
+teleappointment.belongsTo(user, { foreignKey: "user_id", as: "User" });
+teleappointment.belongsTo(doctor, { foreignKey: "doctor_id", as: "Doctor" });
+user.hasMany(appointment, { foreignKey: "user_id" });
+doctor.hasMany(appointment, { foreignKey: "doctor_id" });
---
+++ util/helper.js
@@ -0,0 +1,198 @@
+import { googleappointment as Googleappointment, appointment as Appointment, user as User, doctor as Doctor } from "./db.js";
+
+export async function deregisterDoctor(id) {
+  try {
+    await Appointment.destroy({
+      where: {
+        doctor_id: id,
+      },
+    });
+    console.log("Appointment deleted successfully.");
+    await Doctor.destroy({
+      where: {
+        doctor_id: id,
+      },
+    });
+
+    console.log("Doctor derigestered");
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function updateDoctor(id, name, field, contact, location) {
+  try {
+    await Doctor.update(
+      {
+        name: name,
+        type: field,
+        contact_info: contact,
+        location: location,
+      },
+      {
+        where: {
+          doctor_id: id,
+        },
+      }
+    );
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function cancelAppointment(id) {
+  try {
+    await Appointment.destroy({
+      where: {
+        appointment_id: id,
+      },
+    });
+    console.log("Appointment deleted successfully.");
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function rescheduleAppointment(
+  userId,
+  appointmentId,
+  newDate,
+  newTime
+) {
+  try {
+    const appointment = await Appointment.findOne({
+      where: {
+        user_id: userId,
+        appointment_id: appointmentId,
+      },
+    });
+
+    if (!appointment) {
+      throw new Error("Appointment not found");
+    }
+
+    // Update the appointment attributes
+    appointment.date = newDate;
+    appointment.time = newTime;
+    appointment.status = "rescheduled";
+
+    // Save the changes to the database
+    await appointment.save();
+
+    console.log("Appointment rescheduled successfully");
+  } catch (error) {
+    console.error("Error rescheduling appointment:", error);
+  }
+}
+
+export async function updateGoogleAppointments({
+  doctor_id,
+  date,
+  start_time,
+  end_time,
+  appointment_info,
+  google_id,
+}) {
+  try {
+    const appointment = await Googleappointment.findOne({
+      where: { google_id: google_id },
+    });
+
+    if (appointment) {
+      return null;
+    }
+    const newAppointment = await Googleappointment.create({
+      doctor_id: doctor_id,
+      date: date,
+      start_time: start_time,
+      end_time: end_time,
+      appointment_info: appointment_info,
+      google_id: google_id,
+    });
+
+    return newAppointment;
+  } catch (error) {
+    console.error("Error rescheduling appointment:", error);
+  }
+}
+
+export async function getUserPhoneNumber(userId) {
+  try {
+    const user = await User.findOne({
+      attributes: ["phone_number"],
+      where: {
+        user_id: userId,
+      },
+    });
+    if (user) {
+      return user.dataValues.phone_number;
+    } else {
+      return null; // User not found
+    }
+  } catch (error) {
+    console.error("Error fetching user's phone number:", error);
+    throw error;
+  }
+}
+
+export async function getDocName(docId) {
+  try {
+    const doctor = await Doctor.findOne({
+      attributes: ["name"],
+      where: {
+        doctor_id: docId,
+      },
+    });
+    if (doctor) {
+      return doctor.dataValues.name;
+    } else {
+      return null; // User not found
+    }
+  } catch (error) {
+    console.error("Error fetching user's phone number:", error);
+    throw error;
+  }
+}
+
+export async function createDoctor(
+  docId,
+  name,
+  field,
+  contact,
+  location,
+  email,
+  address
+) {
+  try {
+    const newDoctor = await Doctor.create({
+      doctor_id: docId,
+      name: name,
+      type: field,
+      contact_info: contact,
+      location: location,
+      email: email,
+      address: address,
+    });
+    return newDoctor;
+  } catch (error) {
+    console.error("Error creating doctor:", error);
+    throw error;
+  }
+}
+
+export async function getLastDoctorId() {
+  try {
+    const lastDoctor = await Doctor.findOne({
+      attributes: ["doctor_id"],
+      order: [["doctor_id", "DESC"]],
+    });
+    if (lastDoctor) {
+      return lastDoctor.doctor_id;
+    } else {
+      return null; // No doctors found
+    }
+  } catch (error) {
+    console.error("Error fetching last doctor ID:", error);
+    throw error;
+  }
+}
